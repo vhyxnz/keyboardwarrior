455910a2d9e32778c03e7a96998f9b2c72c1f799
vhyxnz <vhyxnz@gmail.com>
2026-09-03 09:06:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = "keyboard-warrior-pwa-v3";
+const CACHE_NAME = "keyboard-warrior-pwa-v4";
 const APP_SHELL = [
     "./",
     "./index.html",
-    "./index.html?v=3",
+    "./index.html?v=4",
     "./manifest.webmanifest",
     "./KWLogo-v2.png",
     "./icon-180.png",
